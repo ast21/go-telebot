b63b37952fddc6581994619d72e3070b3d2759a5
Anastas Mironov <ast@l80.ru>
2023-07-18 01:18:42 +0600

    feat: save user to db, and get name for welcome text

--- main.go
+++ main.go
@@ -48,18 +48,81 @@ func main() {
 		return
 	}
 
-	b.Handle("/start", func(c tele.Context) error {
-		return c.Send("Hello! I am Learning Assistant. Welcome!")
-	})
-
 	b.Handle("/hello", func(c tele.Context) error {
-		return c.Send("Hello!")
+		return c.Send("Hello! How are you?")
 	})
 
 	// All the text messages that weren't
 	// captured by existing handlers.
 	b.Handle(tele.OnText, func(c tele.Context) error {
 		return c.Send(c.Text())
+	})
+
+	var (
+		// Universal markup builders.
+		menu     = &tele.ReplyMarkup{ResizeKeyboard: true}
+		selector = &tele.ReplyMarkup{}
+
+		// Reply buttons.
+		btnHelp     = menu.Text("ℹ Help")
+		btnSettings = menu.Text("⚙ Settings")
+
+		// Inline buttons.
+		//
+		// Pressing it will cause the client to
+		// send the bot a callback.
+		//
+		// Make sure Unique stays unique as per button kind
+		// since it's required for callback routing to work.
+		//
+		btnPrev = selector.Data("Prev", "prev")
+		btnNext = selector.Data("Next", "next")
+	)
+
+	menu.Reply(
+		menu.Row(btnHelp),
+		menu.Row(btnSettings),
+	)
+	selector.Inline(
+		selector.Row(btnPrev, btnNext),
+	)
+
+	b.Handle("/start", func(c tele.Context) error {
+		sender := c.Sender()
+
+		user := User{}
+		err = db.Get(&user, "SELECT * FROM users WHERE telegram_id = $1", sender.ID)
+		if err != nil {
+			fmt.Println(err)
+
+			tx := db.MustBegin()
+			tx.MustExec("INSERT INTO users (telegram_id, first_name, last_name) VALUES ($1, $2, $3)", sender.ID, sender.FirstName, sender.LastName)
+			err := tx.Commit()
+
+			if err != nil {
+				log.Fatalln(err)
+			}
+
+			err = db.Get(&user, "SELECT * FROM users WHERE telegram_id = $1", sender.ID)
+			if err != nil {
+				log.Fatalln(err)
+			}
+		}
+
+		fmt.Println(user)
+
+		welcome := fmt.Sprintf("Hello %s %s! I am Learning Assistant. Welcome!", user.FirstName, user.LastName)
+		return c.Send(welcome, menu)
+	})
+
+	// On reply button pressed (message)
+	b.Handle(&btnHelp, func(c tele.Context) error {
+		return c.Edit("Here is some help: ...")
+	})
+
+	// On inline button pressed (callback)
+	b.Handle(&btnPrev, func(c tele.Context) error {
+		return c.Respond()
 	})
 
 	b.Start()
