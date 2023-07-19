4f2c284f489b953078ee38524976172a4560b501
Anastas Mironov <ast@l80.ru>
2023-07-19 23:28:39 +0600

    feat: add more structs

--- data/table_structs.go
+++ data/table_structs.go
@@ -10,3 +10,39 @@ type User struct {
 	CreatedAt  time.Time `db:"created_at"`
 	UpdatedAt  time.Time `db:"updated_at"`
 }
+
+type Course struct {
+	ID        uint64    `db:"id"`
+	Title     string    `db:"title"`
+	CreatedAt time.Time `db:"created_at"`
+	UpdatedAt time.Time `db:"updated_at"`
+}
+
+type Question struct {
+	ID        uint64    `db:"id"`
+	Title     string    `db:"title"`
+	CreatedAt time.Time `db:"created_at"`
+	UpdatedAt time.Time `db:"updated_at"`
+
+	Course Course `db:"course"`
+}
+
+type QuestionAnswer struct {
+	ID        uint64    `db:"id"`
+	Value     string    `db:"value"`
+	CreatedAt time.Time `db:"created_at"`
+	UpdatedAt time.Time `db:"updated_at"`
+
+	Question Question `db:"question"`
+}
+
+type UserAnswer struct {
+	ID        uint64    `db:"id"`
+	Value     string    `db:"value"`
+	Correct   bool      `db:"correct"`
+	CreatedAt time.Time `db:"created_at"`
+	UpdatedAt time.Time `db:"updated_at"`
+
+	User     User     `db:"user"`
+	Question Question `db:"question"`
+}
